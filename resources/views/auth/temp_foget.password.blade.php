cb3757b9e2ab942a0cea2c97eef0015fa8c9c210
atsushikano <ash.surfing.life@gmail.com>
2022-01-13 11:40:32 +0900

    basic backend

---
+++ resources/views/auth/temp_foget.password.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Corona Admin</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('../../backend/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('../../backend/assets/vendors/css/vendor.bundle.base.css') }}">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="{{ asset('../../backend/assets/css/style.css') }}">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="{{ asset('../../backend/assets/images/favicon.png') }}" />
+  </head>
+  <body>
+    <div class="container-scroller">
+      <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="row w-100 m-0">
+          <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-bg">
+            <div class="card col-lg-6 mx-auto">
+              <div class="card-body px-5 py-5">
+
+                <div class="mb-6 text-sm text-gray-600">
+                    {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                </div>
+
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+
+                  <div class="flex items-center justify-end mt-4">
+                    <label>Email *</label>
+                    <input id="email" class="block" type="email" name="email" class="form-control p_input">
+                  </div>
+
+                  <div class="flex items-center justify-end mt-4">
+                        {{ __('Email Password Reset Link') }}
+                  </div>
+
+                  <div class="text-center">
+                    <button type="submit" class="btn btn-primary btn-block enter-btn">Submit</button>
+                  </div>
+
+                </form>
+              </div>
+            </div>
+          </div>
+          <!-- content-wrapper ends -->
+        </div>
+        <!-- row ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="{{ asset('../../assets/vendors/js/vendor.bundle.base.js') }}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="{{ asset('../../backend/assets/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('../../backend/assets/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('../../backend/assets/js/misc.js') }}"></script>
+    <script src="{{ asset('../../backend/assets/js/settings.js') }}"></script>
+    <script src="{{ asset('../../backend/assets/js/todolist.js') }}"></script>
+    <!-- endinject -->
+  </body>
+</html>
