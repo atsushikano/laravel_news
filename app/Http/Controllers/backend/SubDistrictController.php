8c8e780d79c1cfe621ad9e9fe9a1842d8e18bc3d
atsushikano <ash.surfing.life@gmail.com>
2022-01-25 01:06:08 +0900

    DistrictCRUD-finished

--- app/Http/Controllers/backend/SubDistrictController.php
+++ app/Http/Controllers/backend/SubDistrictController.php
@@ -3,9 +3,87 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
+
+use function PHPUnit\Framework\returnSelf;
 
 class SubDistrictController extends Controller
 {
-    //
+    public function Index()
+    {
+        $subdistrict = DB::table('subdistricts')
+            ->join('districts', 'subdistricts.district_id', 'districts.id')
+            ->select('subdistricts.*', 'districts.district_en')
+            ->orderBy('id', 'desc')->paginate(3);
+        return view('backend/subdistrict/index', compact('subdistrict'));
+    }
+
+    public function AddSubSubDistrict()
+    {
+        $district = DB::table('districts')->get();
+        return view('backend/subdistrict/create', compact('district'));
+    }
+
+    public function StoreSubDistrict(Request $request)
+    {
+        // Validated
+        $request->validate([
+            'subdistrict_en' => 'required|unique:subdistricts|max:255',
+            'subdistrict_jp' => 'required|unique:subdistricts|max:255',
+        ]);
+
+        // dataset
+        $data = array();
+        $data['subdistrict_en'] = $request->subdistrict_en;
+        $data['subdistrict_jp'] = $request->subdistrict_jp;
+        $data['district_id'] = $request->district_id;
+
+        DB::table('subdistricts')->insert($data);
+
+        $notification = array(
+            'message' => 'SubDistrict Inserted Successfully',
+            'alert-type' => 'error'
+        );
+
+        // ridrect
+        return Redirect()->route('subdistricts')->with($notification);
+    }
+
+    public function EditSubDistrict($id)
+    {
+        $subdistrict = DB::table('subdistricts')->where('id', $id)->first();
+        return view('backend/subdistrict/edit', compact('subdistrict'));
+    }
+
+    public function UpdateSubDistrict(Request $request, $id)
+    {
+        $data = array();
+        $data['subdistrict_en'] = $request->subdistrict_en;
+        $data['subdistrict_jp'] = $request->subdistrict_jp;
+
+        DB::table('subdistricts')->where('id', $id)->update($data);
+
+        $notification = array(
+            'message' => 'SubDistrict Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return Redirect()->route('subdistricts')->with($notification);
+    }
+
+    public function DeleteSubDistrict($id)
+    {
+        DB::table('subdistricts')->where('id', $id)->delete();
+
+        $notification = array(
+            'message' => 'SubDistrict Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return Redirect()->route('subdistricts')->with($notification);
+    }
 }
---
+++ resources/views/backend/subdistrict/edit.blade.php
@@ -0,0 +1,57 @@
+@extends('admin.admin_master')
+@section('admin')
+
+<div class="content-wrapper">
+
+    <div class="row">
+      <div class="col-12 grid-margin stretch-card">
+        <div class="card corona-gradient-card">
+          <div class="card-body py-0 px-0 px-sm-3">
+            <div class="row align-items-center">
+              <div class="col-4 col-sm-3 col-xl-2">
+                <img src="{{asset('backend/assets/images/dashboard/Group126@2x.png')}}" class="gradient-corona-img img-fluid" alt="">
+              </div>
+              <div class="col-5 col-sm-7 col-xl-8 p-0">
+                <h4 class="mb-1 mb-sm-0">Wellcome to Easy News</h4>
+              </div>
+              <div class="col-3 col-sm-2 col-xl-2 pl-0 text-center">
+                <span>
+                  <a href="{{url('/')}}" target="_blank" class="btn btn-outline-light btn-rounded get-started-btn">Visit Frontend ?</a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  <div class="col-md-12 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Edit SubDistrict</h4>
+
+        <form class="forms-sample" method="POST" action="{{ route('update.subdistrict',$subdistrict->id) }}">
+            @csrf
+          <div class="form-group">
+            <label for="exampleInputUsername1">SubDistrict English</label>
+            <input type="text" class="form-control" name="subdistrict_en"  value="{{ $subdistrict->subdistrict_en }}">
+            @error('subdistrict_en')
+            <span class="text-danger">{{ $message }}</span>
+            @enderror
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">SubDistrict Japanese</label>
+            <input type="text" class="form-control" name="subdistrict_jp"  value="{{ $subdistrict->subdistrict_jp }}">
+            @error('subdistrict_jp')
+            <span class="text-danger">{{ $message }}</span>
+            @enderror
+          </div>
+
+          <button type="submit" class="btn btn-primary mr-2">Update</button>
+        </form>
+      </div>
+    </div>
+  </div>
+
+@endsection
